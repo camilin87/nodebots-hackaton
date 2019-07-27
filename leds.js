043ef82039de4fcbd2f2324f9f445b9d2c36abf2
Camilo Sanchez <camilin87@gmail.com>
2019-07-27 12:12:13 -0400

    create blinking leds

---
+++ leds.js
@@ -0,0 +1,17 @@
+var five = require("johnny-five");
+var pins = require("./esp8266Pins");
+var board = new five.Board();
+
+board.on("ready", function() {
+    console.log("READY!");
+
+    var ledRed = new five.Led(pins.d3);
+    var ledWhite = new five.Led(pins.d2);
+    var ledGreen = new five.Led(pins.d4);
+
+    ledRed.blink(500);
+    ledWhite.blink(700);
+    ledGreen.blink(1000);
+
+    console.log("IT'S BLINKING!!!");
+});
