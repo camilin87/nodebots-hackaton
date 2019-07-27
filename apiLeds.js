00e020c99ae3066cf3940959d7c51563fc11653d
Camilo Sanchez <camilin87@gmail.com>
2019-07-27 14:24:13 -0400

    rename the yellow led

--- apiLeds.js
+++ apiLeds.js
@@ -3,19 +3,19 @@ var pins = require("./esp8266Pins");
 module.exports = {
     init: function(five){
         var red = new five.Led(pins.d3);
-        var white = new five.Led(pins.d2);
+        var yellow = new five.Led(pins.d2);
         var green = new five.Led(pins.d4);
         var blue = new five.Led(pins.d1);
         var bigRed = new five.Led(pins.d7);
 
         return {
             red: red,
-            white: white,
+            yellow: yellow,
             green: green,
             blue: blue,
             bigRed: bigRed,
             all: [
-                red, white, green, blue, bigRed
+                red, yellow, green, blue, bigRed
             ]
         };
     }
